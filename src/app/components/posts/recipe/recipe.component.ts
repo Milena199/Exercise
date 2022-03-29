80cd0561cfa77bb95f2bdfe0f996980c14ba17b2
Milena199 <stojcevicmilena@gmail.com>
2022-03-29 16:43:08 +0200

    active route params

--- src/app/components/posts/recipe/recipe.component.ts
+++ src/app/components/posts/recipe/recipe.component.ts
@@ -1,3 +1,4 @@
+import { HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { map, tap } from 'rxjs';
@@ -10,26 +11,27 @@ import { PostService } from 'src/app/services/post.service';
   styleUrls: ['./recipe.component.scss']
 })
 export class RecipeComponent implements OnInit {
-  postId: number = this.router.snapshot.params['id'];
-  // postId: any = this.router.queryParams;
+  postId: number = this.route.snapshot.params['id'];
+  post!: Post;
+  id!: number;
 
-  post: any;
+
   posts: Post[] = [];
   constructor(
     private postService: PostService,
-    private router: ActivatedRoute
+    private route: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
-    console.log(this.postId);
-
-    this.getPost();
+    this.route.params.subscribe(params => {
+      this.postId = params['id'];
+      this.getPost();
+    });
     this.getPosts();
   }
   getPost(): void {
     this.postService.getPost(this.postId).subscribe(res => {
       this.post = res;
-
     })
   }
   getPosts(): void {
