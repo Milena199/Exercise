8da91cf7624850555d898ee47f098f5f4c2b372b
Milena199 <stojcevicmilena@gmail.com>
2022-04-14 22:48:42 +0200

    code cleanup

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.2.5.
+
 ## Backend Development server
 
 ### This project use lightweight backend server with JSON database.
@@ -11,5 +13,3 @@ Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app w
 To run the backend server, you need to install the [JSON Server](https://www.npmjs.com/package/json-server) on the host. There is more information about server startup.
 
 The database file is `db_data.json` located in project root directory.
-
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.2.5.
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { FavoritesComponent } from './components/favorites/favorites.component';
 import { PostsComponent } from './components/posts/posts.component';
-import { CommentsComponent } from './components/posts/recipe/comments/comments.component';
 import { RecipeComponent } from './components/posts/recipe/recipe.component';
 
 const routes: Routes = [
-  {
-    path: 'dashboard',
-    component: DashboardComponent
-  },
   {
     path: 'posts',
     component: PostsComponent
@@ -18,9 +12,6 @@ const routes: Routes = [
   {
     path: 'favorites',
     component: FavoritesComponent
-  }, {
-    path: 'comments',
-    component: CommentsComponent,
   },
   {
     path: 'recipe/:id',
