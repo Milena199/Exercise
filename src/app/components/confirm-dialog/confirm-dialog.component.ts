f87785d004a4f5028e3a2f429d33d35b13e7939c
Milena199 <stojcevicmilena@gmail.com>
2022-04-14 22:16:47 +0200

    delete comment dialog added

--- src/app/components/confirm-dialog/confirm-dialog.component.ts
+++ src/app/components/confirm-dialog/confirm-dialog.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
-import { Comments } from 'src/app/models/models';
-import { PostService } from 'src/app/services/post.service';
 
 @Component({
   selector: 'app-confirm-dialog',
@@ -10,23 +7,13 @@ import { PostService } from 'src/app/services/post.service';
   styleUrls: ['./confirm-dialog.component.scss']
 })
 export class ConfirmDialogComponent {
-  commentsData: Comments[] = [];
-  form: FormGroup = this.fb.group({
-    name: ['', Validators.required],
-    email: ['', Validators.email],
-    comment: [''],
-  });
 
   constructor(
-    private postService: PostService,
-    private fb: FormBuilder,
     public dialogRef: MatDialogRef<ConfirmDialogComponent>,
   ) { }
 
-  onSubmit(): void {
-    this.postService.createComment(this.form.value).subscribe(() => {
-      this.dialogRef.close(true);
-    })
-  }
 
+  onClose(result: boolean): void {
+    this.dialogRef.close(result);
+  }
 }
--- src/app/components/posts/recipe/comments/comments.component.ts
+++ src/app/components/posts/recipe/comments/comments.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmDialogComponent } from 'src/app/components/confirm-dialog/confirm-dialog.component';
 import { Comments } from 'src/app/models/models';
 import { PostService } from 'src/app/services/post.service';
 
@@ -13,15 +15,19 @@ export class CommentsComponent {
 
 
   constructor(
-    private postService: PostService
+    private postService: PostService,
+    private dialog: MatDialog
   ) { }
 
 
   deleteCom(comment: Comments): void {
-    this.postService.delete(comment.id as number).subscribe(() => {
-      // refresh comments
-      this.refreshComments();
-    });
+    this.dialog.open(ConfirmDialogComponent).afterClosed().subscribe((result: boolean) => {
+      if (!result) { return; }
+      this.postService.delete(comment.id as number).subscribe(() => {
+        // refresh comments
+        this.refreshComments();
+      });
+    })
   }
 
 }
